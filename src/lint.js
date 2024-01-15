b2ef90ced371a6b4b686007fdbfe9b8faf34b5be
Anoop M D <anoop.md1421@gmail.com>
2024-01-16 02:06:31 +0530

    feat: improved linter

--- src/lint.js
+++ src/lint.js
@@ -1,41 +1,83 @@
 const linter = (input) => {
+  const getClosingBracket = (openBracket) => {
+    switch (openBracket) {
+      case '{': return '}';
+      case '[': return ']';
+      case '(': return ')';
+      default: return '';
+    }
+  };
 
-  const lint = (lines, depth, lineNum, errors) => {
+  const isMatchingBracket = (openBracket, closeBracket) => {
+    return getClosingBracket(openBracket) === closeBracket;
+  };
+
+  const lint = (lines, depth, lineNum, openBrackets) => {
     if (!lines.length) {
-      return errors;
+      if (openBrackets.length > 0) {
+        const lastOpenBracket = openBrackets.pop();
+        return {
+          type: 'syntax',
+          message: `Expected ${getClosingBracket(lastOpenBracket)} but found end of file`,
+          line: lineNum - 1 // lineNum is already incremented
+        };
+      }
+      return null;
     }
 
     const currentLine = lines.shift();
 
     // if the line starts with a #, it's a comment
     if (currentLine.startsWith('#')) {
-      return lint(lines, depth, lineNum + 1, errors);
+      return lint(lines, depth, lineNum + 1, openBrackets);
     }
 
     // ignore empty lines
     if (currentLine === '') {
-      return lint(lines, depth, lineNum + 1, errors);
+      return lint(lines, depth, lineNum + 1, openBrackets);
+    }
+
+    // if the line ends with { or [ or (, it's a composite data type
+    if ([ '{', '[', '(' ].includes(currentLine.slice(-1))) {
+      openBrackets.push(currentLine.trim().slice(-1));
+
+      return lint(lines, depth + 1, lineNum + 1, openBrackets);
+    }
+
+    // if the line starts with } or ] or ), it's the closing bracket
+    if ([ '}', ']', ')' ].includes(currentLine.trim())) {
+      const lastOpenBracket = openBrackets.pop();
+      if (!lastOpenBracket || !isMatchingBracket(lastOpenBracket, currentLine.trim())) {
+        return {
+          type: 'syntax',
+          message: `Expected ${getClosingBracket(lastOpenBracket)} but found ${currentLine.trim()}`,
+          line: lineNum
+        };
+      }
+
+      depth -= 1;
     }
 
     // check indentation
     const expectedIndentation = depth * 2;
     const actualIndentation = currentLine.search(/\S/);
     if (expectedIndentation !== actualIndentation) {
-      errors.push({
+      return {
         type: 'indentation',
+        message: `Expected indentation of ${expectedIndentation} spaces but found ${actualIndentation}`,
         line: lineNum
-      });
+      };
     }
 
-    return lint(lines, depth, lineNum + 1, errors);
+    return lint(lines, depth, lineNum + 1, openBrackets);
   };
 
   const lines = input.split('\n');
-  const errors = [];
   const depth = 0;
   const lineNum = 1;
+  const openBrackets = [];
 
-  return lint(lines, depth, lineNum, errors);
+  return lint(lines, depth, lineNum, openBrackets);
 };
 
 module.exports = linter;
--- tests/lint/indentation.spec.js
+++ tests/lint/indentation.spec.js
@@ -5,10 +5,93 @@ describe('bru lint()', () => {
     const input = `
   name: 'Bruno'
 `
-    const expected = [{
+    const expected = {
       type: 'indentation',
+      message: 'Expected indentation of 0 spaces but found 2',
       line: 2
-    }]
+    };
+
+    const error = lint(input);
+    expect(error).toEqual(expected);
+  });
+
+it('should catch indentation errors due to spaces before the key', () => {
+    const input = `
+person: {
+   name: 'Bruno'
+}
+`
+    const expected = {
+      type: 'indentation',
+      message: 'Expected indentation of 2 spaces but found 3',
+      line: 3
+    };
+
+    const errors = lint(input);
+    expect(errors).toEqual(expected);
+  });
+
+  it('should catch errors due to not closing the composite types correctly', () => {
+    const input = `
+person: {
+  name: 'Bruno'
+`
+    const expected = {
+      type: 'syntax',
+      message: 'Expected } but found end of file',
+      line: 4
+    };
+
+    const errors = lint(input);
+    expect(errors).toEqual(expected);
+  });
+
+  it('should catch errors due to mismatched closing brackets', () => {
+    const input = `
+person: {
+  name: 'Bruno'
+]`
+    const expected = {
+      type: 'syntax',
+      message: 'Expected } but found ]',
+      line: 4
+    };
+
+    const errors = lint(input);
+    expect(errors).toEqual(expected);
+  });
+
+  it('should pass when there are no errors', () => {
+    const input = `
+person: {
+  name: 'Bruno'
+  age: 21
+  hobbies: [
+    'programming'
+    'gaming'
+  ]
+  links: [
+    {
+      name: 'GitHub'
+      url: 'github.com/usebruno/bruno'
+    }
+    {
+      name: 'Twitter'
+      url: 'twitter.com/use_bruno'
+    }
+  ]
+  projects: [
+    {
+      name: 'Bruno'
+      url: 'github.com/usebruno/bruno'
+      description: (
+        Opensource API Client!
+      )
+    }
+  ]
+}
+`
+    const expected = null;
 
     const errors = lint(input);
     expect(errors).toEqual(expected);
